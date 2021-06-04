aaa0db2c2b3da197289325a6f33b44790bb8d37d
ekienay <ekienay@gmail.com>
2021-06-04 17:37:53 +0300

    done

--- src/main/java/com/unnurnment/Application.java
+++ src/main/java/com/unnurnment/Application.java
@@ -2,8 +2,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Run {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Run.class,args);
+        SpringApplication.run(Application.class,args);
     }
 }
